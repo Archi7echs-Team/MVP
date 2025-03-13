7626d64d01857b697ba85c7eb773fdc93cc6bd71
Pyer23 <channelpla@gmail.com>
2025-03-13 16:18:57 +0100

    Fix posizione testo hover

--- app/src/lib/components/Bar.svelte
+++ app/src/lib/components/Bar.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
-    import { T } from '@threlte/core'
-    import {interactivity,Text} from '@threlte/extras'
-	import { writable } from 'svelte/store';
-    export let coordinates = [0, 0, 0];
-    export let height = 1;
-    let over = writable(false)
-    interactivity()
-</script>
+  import { T } from '@threlte/core';
+  import { interactivity, Text } from '@threlte/extras';
   
-<T.Mesh position={coordinates as [number, number, number]} scale={[1, height, 1]} 
-onpointerover={(e: PointerEvent) => {over.set(true); e.stopPropagation();}}  onpointerout={(e: PointerEvent) => {over.set(false); e.stopPropagation();}} >
-    <T.BoxGeometry args={[1, 1, 1]} />
-    <T.MeshStandardMaterial color={`hsl(${((coordinates[2] + 360) * 30) % 360}, 80%, 60%)`} />
-    {#if $over == true}
-    <Text position.x={coordinates[0]} position.y={height + 0.25} position.z={coordinates[2]} text={height.toFixed(2)} color="black" anchorX="center" anchorY="middle"/>
-    {/if}
+  let {coordinates, height} = $props();
+  let over = $state(false);
+
+  let textPosition = [0, coordinates[1] + height + 1, 0];
+
+  interactivity();
+
+</script>
+
+<T.Mesh 
+position={coordinates as [number, number, number]} 
+scale={[1, height, 1]} 
+raycastFirst={true}
+onpointerover={(e: PointerEvent) => { over = true; e.stopPropagation(); }}  
+onpointerout={(e: PointerEvent) => { over = false; e.stopPropagation(); }}
+>
+<T.BoxGeometry args={[1, 1, 1]} />
+<T.MeshStandardMaterial color={`hsl(${((coordinates[2] + 360) * 30) % 360}, 80%, 60%)`} />
 </T.Mesh>
+
+{#if over}
+  <Text 
+  position={[coordinates[0], height + 0.25,  coordinates[2]]}   
+  text={height.toFixed(2)} 
+    color="black" 
+    anchorX="center" 
+    anchorY="middle" 
+  />
+{/if}
+
+
+
+
