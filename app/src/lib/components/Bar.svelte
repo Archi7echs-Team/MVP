9574f783fd5a9a37258279b5ec692f1ae233fa1b
Gabriele Checchinato <gabrielechecchinato03@gmail.com>
2025-03-14 10:19:57 +0100

    Text OK

--- app/src/lib/components/Bar.svelte
+++ app/src/lib/components/Bar.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
-  import { T } from '@threlte/core';
+  import { T, useThrelte } from '@threlte/core';
   import { interactivity, Text } from '@threlte/extras';
-  import { useThrelte } from '@threlte/core';
+  import { onMount, onDestroy } from 'svelte';
 
   let {coordinates, height} = $props();
   let over = $state(false)
-
+  let camQuat = $state<[number, number, number, number]>([0, 0, 0, 1]);
+  let animationFrameId: number;
   const { camera } = useThrelte();
+
+  onMount(() => {
+    const update = () => {
+      if (camera?.current) {
+        camQuat = [
+          camera.current.quaternion.x,
+          camera.current.quaternion.y,
+          camera.current.quaternion.z,
+          camera.current.quaternion.w
+        ];
+      }
+      animationFrameId = requestAnimationFrame(update);
+    };
+    update();
+  });
+
+  onDestroy(() => {
+    cancelAnimationFrame(animationFrameId);
+  });
 
   interactivity();
 
@@ -15,8 +35,8 @@
 <T.Mesh 
 position={coordinates as [number, number, number]} 
 scale={[1, height, 1]} 
-onpointerover={(e: PointerEvent) => { over = true; e.stopPropagation(); }}  
-onpointerout={(e: PointerEvent) => { over = false; e.stopPropagation(); }}
+onpointerover={(e: PointerEvent) => { e.stopPropagation(); over = true; }}  
+onpointerout={(e: PointerEvent) => { e.stopPropagation(); over = false;}}
 >
 <T.BoxGeometry args={[1, 1, 1]} />
 <T.MeshStandardMaterial color={`hsl(${((coordinates[2] + 360) * 30) % 360}, 80%, 60%)`} />
@@ -29,6 +49,6 @@ onpointerout={(e: PointerEvent) => { over = false; e.stopPropagation(); }}
     color="black" 
     anchorX="center" 
     anchorY="middle"
-    quaternion={[camera.current.quaternion.x, camera.current.quaternion.y, camera.current.quaternion.z, camera.current.quaternion.w]}
+    quaternion={camQuat}
   />
 {/if} 
--- app/src/lib/components/Scene.svelte
+++ app/src/lib/components/Scene.svelte
@@ -4,13 +4,12 @@
   import Chart from './Chart.svelte';
 </script>
 
-    <!-- Camera -->
-    <T.PerspectiveCamera position={[0, 5, 10]} makeDefault >
-      <OrbitControls enableDamping maxPolarAngle={1.55}/>
-    </T.PerspectiveCamera>
-    <!-- Lights -->
-    <T.AmbientLight intensity={0.5} />
-    <T.DirectionalLight castShadow position={[5, 10, 5]} intensity={1} />
+  <!-- Camera -->
+  <T.PerspectiveCamera position={[0, 5, 10]} makeDefault >
+    <OrbitControls enableDamping maxPolarAngle={1.55}/>
+  </T.PerspectiveCamera>
+  <!-- Lights -->
+  <T.AmbientLight intensity={0.5} />
+  <T.DirectionalLight castShadow position={[5, 10, 5]} intensity={1} />
   
-
-<Chart />
+  <Chart />
