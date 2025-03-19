1678acfd8d0e9b6f34aa90808281b279f2855c05
Francesco Pozzobon <francesco.pozzobon.1@studenti.unipd.it>
2025-03-19 16:29:01 +0100

    altra mod

--- app/src/lib/components/DataRange.svelte
+++ app/src/lib/components/DataRange.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import { Button, Pane, Text } from 'svelte-tweakpane-ui';
-    import { writable } from 'svelte/store';
 
-    export let minVal = writable("0");
-    export let maxVal = writable("100");
+    let minVal = "0";
+    let maxVal = "0";
 </script>
 
-<Text label="Valore Min" bind:value={$minVal} />
-<Text label="Valore Max" bind:value={$maxVal} />
+<Text label="Valore Min" bind:value={minVal} />
+<Text label="Valore Max" bind:value={maxVal} />
 
 
   
