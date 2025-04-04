2956ae61db110ac7abf601dc6f905ccad25babff
Giacomo Pesenato <giacomopesenato@gmail.com>
2025-04-04 17:19:20 +0200

    fix test

--- backend/src/main/java/com/dataviz/backend/controller/UploadController.java
+++ backend/src/main/java/com/dataviz/backend/controller/UploadController.java
@@ -1,18 +1,15 @@
 package com.dataviz.backend.controller;
 
-import com.dataviz.backend.exception.FileTooBigException;
-import com.dataviz.backend.exception.InvalidCsvException;
+
 import com.dataviz.backend.model.MatrixData;
 import com.dataviz.backend.service.CsvFileReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -28,8 +25,7 @@ public class UploadController {
     }
 
     @PostMapping("/uploadCsv")
-    public ResponseEntity<?> uploadCsv(@RequestParam("file") MultipartFile file) {
-        MatrixData csvParsed = csvFileReader.parseCsv(file);
-        return ResponseEntity.ok("File uploaded and parsed successfully.");
+    public MatrixData uploadCsv(@RequestParam("file") MultipartFile file) {
+        return csvFileReader.parseCsv(file);
     }
 }
